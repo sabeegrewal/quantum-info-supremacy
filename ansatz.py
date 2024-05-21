4d372f62d92198ba34fc7c69b176b1d1cac76310
William Kretschmer <william-kretschmer@comcast.net>
2024-05-21 13:13:56 -0700

    Added random disjoint matching architecture

--- ansatz.py
+++ ansatz.py
@@ -7,6 +7,30 @@ random.seed(40)
 qu.gen.rand.seed_rand(40)
 
 
+def random_disjoint_matching(N, prev_pairs):
+    prev_pairs = prev_pairs.copy()
+    random.shuffle(prev_pairs)
+    for i in range(len(prev_pairs)):
+        prev_pairs[i] = list(prev_pairs[i])
+        random.shuffle(prev_pairs[i])
+    
+    out = []
+    if N % 2 == 0:
+        head, tail = prev_pairs.pop()
+    else:
+        # Missing index
+        head = (N * (N - 1) // 2) - sum(sum(pair) for pair in prev_pairs)
+        
+    for i, j in prev_pairs:
+        out.append((head, i))
+        head = j
+
+    if N % 2 == 0:
+        out.append((head, tail))
+
+    return out
+    
+
 def random_matching(N):
     """Generate a random maximal set of disjoint pairs from the set {0, ..., N-1}"""
     S = set(range(N))
@@ -35,14 +59,8 @@ def single_qubit_layer(circ, gate_round=None):
         circ.apply_gate("U3", *params, i, gate_round=gate_round, parametrize=True)
 
 
-def two_qubit_layer(circ, two_qubit_mode="random_matching", gate_round=None):
+def two_qubit_layer(circ, pairs, gate_round=None):
     """Apply aparametrizable layer of two qubit ``RZZ`` gates."""
-    if two_qubit_mode is "random_matching":
-        pairs = random_matching(circ.N)
-    if two_qubit_mode is "brickwork":
-        assert gate_round is not None
-        pairs = brickwork(circ.N, s=gate_round)
-
     for i, j in pairs:
         # initialize with random parameters
         params = qu.randn(1, dist="uniform")
@@ -54,14 +72,24 @@ def ansatz_circuit(n, depth, two_qubit_mode="random_matching", **kwargs):
     assert n > 1
     circ = qtn.Circuit(n, **kwargs)
 
-    for r in range(depth):
+    for gate_round in range(depth):
         # single qubit gate layer
-        single_qubit_layer(circ, gate_round=r)
+        single_qubit_layer(circ, gate_round=gate_round)
 
-        # alternate between forward and backward CZ layers
-        two_qubit_layer(circ, two_qubit_mode, gate_round=r)
+        if two_qubit_mode == "random_matching":
+            pairs = random_matching(circ.N)
+        elif two_qubit_mode == "brickwork":
+            pairs = brickwork(circ.N, s=gate_round)
+        elif two_qubit_mode == "random_disjoint":
+            if gate_round == 0:
+                pairs = random_matching(circ.N)
+            else:
+                pairs = random_disjoint_matching(circ.N, pairs)
+            
+        # apply RZZ layers
+        two_qubit_layer(circ, pairs, gate_round=gate_round)
 
     # add a final single qubit layer
-    single_qubit_layer(circ, gate_round=r + 1)
+    single_qubit_layer(circ, gate_round=depth + 1)
 
     return circ
