674559be02647216f7654bed1070667f45baa45c
William Kretschmer <william-kretschmer@comcast.net>
2025-03-04 16:30:10 -0800

    Faster ansatz computation using jax alone
    The idea is to first multiply the 1- and 2-qubit gates together so that we reduce the total number of tensor contractions needed.
    
    Note that this also switches to using the Quantinuum conventions for gate parametrization.

--- ansatz/ansatz_jax.py
+++ ansatz/ansatz_jax.py
@@ -1,5 +1,5 @@
+import jax
 import jax.numpy as jnp
-
 
 def u3(theta, phi, lamda):
     """Arbitrary one-qubit gate with three real parameters.
@@ -16,13 +16,13 @@ def u3(theta, phi, lamda):
         The U3 gate parametrized by (theta, phi, lamda) as a jax array of shape (2, 2).
     """
 
-    theta_2 = theta / 2
-    ct2 = jnp.cos(theta_2)
-    st2 = jnp.sin(theta_2)
-    eil = jnp.exp(1j * lamda)
-    eip = jnp.exp(1j * phi)
-    # See https://docs.quantum.ibm.com/api/qiskit/qiskit.circuit.library.U3Gate
-    return jnp.array([[ct2, -eil * st2], [eip * st2, eip * eil * ct2]])
+    pitheta_2 = (jnp.pi/2) * theta
+    cpt2 = jnp.cos(pitheta_2)
+    spt2 = jnp.sin(pitheta_2)
+    eipl = jnp.exp((jnp.pi * 1j) * lamda)
+    eipp = jnp.exp((jnp.pi * 1j) * phi)
+    # See https://docs.quantinuum.com/tket/api-docs/optype.html
+    return jnp.array([[cpt2, -eipl * spt2], [eipp * spt2, eipp * eipl * cpt2]])
 
 
 def rzz(theta):
@@ -39,118 +39,38 @@ def rzz(theta):
         The RZZ gate parametrized by theta as a jax array of shape (4, 4).
     """
 
-    eit2 = jnp.exp(-0.5j * theta)
-    eit2c = eit2.conj()
-    # See https://docs.quantum.ibm.com/api/qiskit/qiskit.circuit.library.RZZGate
-    return jnp.diag(jnp.array([eit2, eit2c, eit2c, eit2]))
+    eipt2 = jnp.exp((-jnp.pi*0.5j) * theta)
+    eipt2c = eipt2.conj()
+    # See https://docs.quantinuum.com/tket/api-docs/optype.html
+    return jnp.diag(jnp.array([eipt2, eipt2c, eipt2c, eipt2]))
 
-
-def apply_u3(state, i, theta, phi, lamda):
-    """Apply a single U3 gate.
-
-    Parameters
-    ----------
-    state : jax array
-        Should have shape `[2] * n` for some `n > i`.
-    i : int
-        Index at which to apply the gate.
-    theta : real
-    phi  : real
-    lamda : real
-
-    Returns
-    -------
-    jax array
-        The state with the U3(theta, phi, lamda) gate applied at index i.
-    """
-
-    U = u3(theta, phi, lamda)
-    # This will roll the index i to the end
-    state = jnp.tensordot(state, U, axes=([i], [1]))
-    # Now we need to roll the last index back to i
-    state = jnp.rollaxis(state, -1, i)
-    return state
-
-
-def apply_rzz(state, i, j, theta):
-    """Apply a single RZZ gate.
+def apply_two_qubit(state, gate, i, j):
+    """Apply a 2-qubit gate.
 
     Parameters
     ----------
     state : jax array
         Should have shape `[2] * n` for some `n > j`.
+    gate : jax array
+        A 2-qubit gate as a jax array of shape (2, 2, 2, 2).
     i : int
         First index at which to apply the gate.`
     j : int
         Second index at which to apply the gate.
-    theta : real
-
     Returns
     -------
     jax array
-        The state with the RZZ(theta) gate applied at indices (i, j).
+        The state with the given gate gate applied at indices (i, j).
     """
 
-    U = rzz(theta).reshape([2, 2, 2, 2])
-    # Set i to the smaller and j to the larger index,
-    # so that index rolling works as intended
-    i, j = sorted((i, j))
-    # This will roll the indices i, j to the end
-    state = jnp.tensordot(state, U, axes=([i, j], [2, 3]))
-    # Now we need to roll the last two indices back to i, j
-    state = jnp.rollaxis(state, -2, i)
-    state = jnp.rollaxis(state, -1, j)
-    return state
-
-
-def one_qubit_layer(one_qubit_params, state, indices):
-    """Apply a layer of U3 gates.
-
-    Parameters
-    ----------
-    one_qubit_params : jax array
-        Parameters of the gates. Should have length `3*len(indices)`.
-    state : jax array
-        Should have shape `[2] * n` for some `n > max(indices)`.
-    indices : list[int]
-        Indices at which to apply gates.
-
-    Returns
-    -------
-    jax array
-        The state with the corresponding U3 gates applied.
-    """
-
-    for idx in range(len(indices)):
-        theta, phi, lamda = one_qubit_params[3 * idx : 3 * idx + 3]
-        i = indices[idx]
-        state = apply_u3(state, i, theta, phi, lamda)
-    return state
-
-
-def two_qubit_layer(two_qubit_params, state, indices):
-    """Apply a layer of RZZ gates.
-
-    Parameters
-    ----------
-    two_qubit_params : jax array
-        Parameters of the gates. Should have length `len(indices)`.
-    state : jax array
-        Should have shape `[2] * n` for some `n > max(max(pair) for pair in indices)`.
-    indices : list[(int, int)]
-        Pairs of indices at which to apply gates.
-
-    Returns
-    -------
-    jax array
-        The state with the corresponding RZZ gates applied.
-    """
-
-    for idx in range(len(indices)):
-        theta = two_qubit_params[idx]
-        i, j = indices[idx]
-        state = apply_rzz(state, i, j, theta)
-    return state
+    # Names for indices into the state tensor
+    indices = "abcdefghijklmnopqrstuvwxyz"[:len(state.shape)]
+    # Replace names at locations i and j with "y" and "z", respectively
+    new_indices = indices[:i] + "y" + indices[i+1:]
+    new_indices = new_indices[:j] + "z" + new_indices[j+1:]
+    # Contract indices[i] and indices[j], replacing with "y" and "z", respectively
+    einsum_str = indices + ",yz" + indices[i] + indices[j] + "->" + new_indices
+    return jnp.einsum(einsum_str, state, gate)
 
 
 def brickwork_pairs(num_qubits, layer):
@@ -173,142 +93,132 @@ def brickwork_pairs(num_qubits, layer):
         for i in range(0, num_qubits - 1, 2)
     ]
 
-
-def num_ansatz_params(num_qubits, depth):
-    """Numper of continuous parameters in an ansatz circuit. This is for a
-    1D cyclic brickwork architecture with alternating U3 and RZZ gates.
+def state2(theta, phi):
+    """Arbitrary one-qubit state with two real parameters.
 
     Parameters
     ----------
-    num_qubits : int
-        Number of qubits in the circuit.
-    depth : int
-        Depth of the circuit, as measured by number of two-qubit layers.
-
-    Returns
-    -------
-    int
-        The number of parameters needed to build such an ansatz circuit.
-    """
-
-    num_2_per_layer = num_qubits // 2
-    num_1 = num_qubits + 2 * num_2_per_layer * depth
-    num_2 = num_2_per_layer * depth
-    return num_2 + 3 * num_1
-
-
-def ansatz_state(params, num_qubits, depth):
-    """Compute the output state of a 1D ansatz circuit with given parameters.
-    This is for a 1D cyclic brickwork architecture with alternating U3 and RZZ gates.
-    The circuit is initialized to the all zero state.
-
-    Parameters
-    ----------
-    params : jax array
-        Parameters of the gates. Should have length `num_ansatz_params(num_qubits, depth)`.
-    num_qubits : int
-        Number of qubits in the circuit and its output state.
-    depth : int
-        Depth of the ansatz circuit, as measured by number of two-qubit layers.
+    theta : real
+    phi  : real
 
     Returns
     -------
     jax array
-        The state with the parametrized ansatz circuit applied, as an array of shape `[2] * num_qubits`.
+        The one-qubit state equivalent to U3(theta, phi, _)|0> as a jax array of shape `(2)`.
     """
 
-    # Initialize the all-zero state
-    state = jnp.zeros(2**num_qubits)
-    state = state.at[0].set(1)
-    state = state.reshape([2] * num_qubits)
+    # Matching the convention of Quantinuum: https://tket.quantinuum.com/api-docs/optype.html
+    pitheta_2 = (jnp.pi / 2) * theta
+    cpt2 = jnp.cos(pitheta_2)
+    spt2 = jnp.sin(pitheta_2)
+    eipp = jnp.exp((jnp.pi * 1j) * phi)
+    return jnp.array([cpt2, eipp * spt2])
 
-    num_2_per_layer = num_qubits // 2
-    num_2 = num_2_per_layer * depth
-    # Split the parameter array into the RZZ and U3 parameters
-    rzz_params = params[:num_2]
-    u3_params = params[num_2:]
+def apply_circuit(depth, initial_state, params):
+    """Apply a 1D ansatz circuit with given parameters to the initial state.
+    This is for a 1D cyclic brickwork architecture with alternating U3 and RZZ gates.
 
-    # Start with a one-qubit layer on all qubits
-    state = one_qubit_layer(u3_params[: 3 * num_qubits], state, range(num_qubits))
+    Parameters
+    ----------
+    depth : int
+        Depth of the ansatz circuit, as measured by number of two-qubit layers.
+    initial_state : jax array
+        Quantum state to apply the circuit to.
+        Should have shape `[2] * n` for some `n`.
+    params : jax array
+        List of real parameters that define the ansatz circuit.
+        Should have length `(n // 2) * depth * 7`.
 
+    Returns
+    -------
+    jax array
+        The output of the ansatz circuit applied to the given state.
+    """
+
+    # Infer the number of qubits
+    num_qubits = len(initial_state.shape)
+    
+    # Number of parameters in RZZ gates
+    num_rzz_params = (num_qubits // 2) * depth
+
+    # Offsets into the parameter array
+    rzz_off = 0
+    u3_off = num_rzz_params
+
+    state = initial_state
     for layer in range(depth):
         # First identify the paired qubits
         pairs = brickwork_pairs(num_qubits, layer)
 
-        # Apply an RZZ layer
-        rzz_off = layer * num_2_per_layer
-        state = two_qubit_layer(
-            rzz_params[rzz_off : rzz_off + num_2_per_layer], state, pairs
-        )
+        # Apply 2- and 1-qubit gates to each pair
+        for i, j in pairs:
+            # RZZ gate to apply
+            rzz_gate = rzz(params[rzz_off])
+            rzz_off += 1
+            # U3 gates to apply
+            u3_gate_i = u3(params[u3_off],   params[u3_off+1], params[u3_off+2])
+            u3_gate_j = u3(params[u3_off+3], params[u3_off+4], params[u3_off+5])
+            u3_off += 6
 
-        # Apply a U3 layer, but only to the qubits that were paired
-        indices = list(sum(pairs, ()))  # This flattens the list of paired indices
-        u3_off = (3 * num_qubits) + (6 * layer * num_2_per_layer)
-        state = one_qubit_layer(
-            u3_params[u3_off : u3_off + 6 * num_2_per_layer], state, indices
-        )
+            # Multiply the 2- and 1-qubit gates together first
+            # This reduces the total number of tensor contractions by a factor of 3
+            u3_gates = jnp.kron(u3_gate_i, u3_gate_j)
+            gate_matrix = jnp.matmul(u3_gates, rzz_gate)
+            gate_tensor = gate_matrix.reshape([2,2,2,2])
 
+            # Apply the combined gate
+            state = apply_two_qubit(state, gate_tensor, i, j)
+            
     return state
 
-
-def ansatz_circ_quimb(params, num_qubits, depth):
-    """Compute the 1D ansatz circuit with given parameters, using quimb.
-    This is for a 1D cyclic brickwork architecture with alternating U3 and RZZ gates.
-    The circuit is initialized to the all zero state.
-    Useful as a sanity check for `ansatz_state`.
+def product_state(params):
+    """Arbitrary product state with given parameters.
 
     Parameters
     ----------
     params : jax array
-        Parameters of the gates. Should have length `num_ansatz_params(num_qubits, depth)`.
-    num_qubits : int
-        Number of qubits in the circuit and its output state.
-    depth : int
-        Depth of the ansatz circuit, as measured by number of two-qubit layers.
+        Should have shape `(2 * n)` for some `n`.
 
     Returns
     -------
-    quimb Circuit
-        The parametrized ansatz circuit.
+    jax array
+        The corresponding product state as a jax array of shape `[2] * n`.
     """
-    import quimb as qu
-    import quimb.tensor as qtn
 
-    circ = qtn.Circuit(num_qubits)
+    # Infer the first dimension, which is the number of qubits
+    params = params.reshape(-1, 2)
+    result = 1
+    for theta, phi in params:
+        result = jnp.tensordot(result, state2(theta, phi), axes=[[], []])
+    return result
 
-    num_2_per_layer = num_qubits // 2
-    num_2 = num_2_per_layer * depth
-    # Split the parameter array into the RZZ and U3 parameters
-    rzz_params = params[:num_2]
-    u3_params = params[num_2:]
+def zzphase_fidelity(theta):
+    """Approximate fidelity with which we can implement a ZZ gate with given parameter.
 
-    # Current offset into the parameter arrays
-    u3_off = 0
-    rzz_off = 0
+    Parameters
+    ----------
+    theta : real or array
+        If given an array of thetas, this method returns the array of corresponding fidelities.
 
-    # Start with a one-qubit layer on all qubits
-    for i in range(num_qubits):
-        theta, phi, lamda = u3_params[u3_off : u3_off + 3]
-        u3_off += 3
-        circ.apply_gate("U3", theta, phi, lamda, i, gate_round=0, parametrize=True)
+    Returns
+    -------
+    real or array
+        The fidelity (or fidelities) corresponding to theta.
+    """
 
-    for layer in range(depth):
-        # First identify the paired qubits
-        pairs = brickwork_pairs(num_qubits, layer)
+    # In qujax, the maximally entangling ZZ gate has theta = 0.5;
+    # See https://cqcl.github.io/qujax/gates.html
 
-        # Apply an RZZ layer
-        for i, j in pairs:
-            theta = rzz_params[rzz_off]
-            rzz_off += 1
-            circ.apply_gate("RZZ", theta, i, j, gate_round=layer, parametrize=True)
-
-        # Apply a U3 layer, but only to the qubits that were paired
-        indices = list(sum(pairs, ()))  # This flattens the list of paired indices
-        for i in indices:
-            theta, phi, lamda = u3_params[u3_off : u3_off + 3]
-            u3_off += 3
-            circ.apply_gate(
-                "U3", theta, phi, lamda, i, gate_round=layer + 1, parametrize=True
-            )
-
-    return circ
+    # First normalize theta to [0, 1), which is equivalent to applying Z gates after if necessary
+    theta = jnp.mod(theta, 1)
+    # Then normalize to [0, 1/2), which is equivalent to conjugating one qubit by X if necessary
+    theta = 1 / 2 - jnp.abs(theta - 1 / 2)
+    # Estimate of 2-qubit gate error rate
+    eps_tq = 0.00148 * theta + 0.00027
+    # Estimate of 1-qubit memory error rate
+    eps_mem = 8e-5
+    # Finally compute a linear function
+    # The reason for the 5/4 and 3 is a (d+1)/d in going from average fidelity to process fidelity
+    # For 2-qubit gates d = 4, for 1-qubit gates d = 2
+    # Finally, we account for eps_mem twice, making 3/2 -> 3
+    return 1 - (5/4) * eps_tq - 3 * eps_mem
