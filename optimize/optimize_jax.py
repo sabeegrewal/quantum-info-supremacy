3f6b876c0635bcc67f0c3a5cc1c30d63f123c360
William Kretschmer <william-kretschmer@comcast.net>
2025-03-06 20:38:18 -0800

    Better random initialization of parameters
    I've done a bit of testing and found that initializing the U3 parameters Haar-randomly and setting the RZZ parameters to 0 yields (slightly) better fidelity than any other initialization method I've tried. So, this will be the new default initialization.

--- optimize/optimize_jax.py
+++ optimize/optimize_jax.py
@@ -154,6 +154,68 @@ noisy_loss_and_grad = jax.jit(jax.value_and_grad(noisy_loss))
 # Optimization
 # ------------
 
+def haar_u3_params():
+    """
+    Generate parameters for a Haar-random U3 gate.
+
+    Returns
+    -------
+    (real, real, real)
+        (theta, phi, lamda) distributed such that u3(theta, phi, lambda)
+        equals the one-qubit unitary Haar measure, up to global phase.
+    """
+
+    # TODO: optionally take a seed as input
+
+    # This method uses properties of the Euler angle parametrization of SU(2)
+    # See Section 2.3 of:
+    # http://home.lu.lv/~sd20008/papers/essays/Random%20unitary%20[paper].pdf
+    # sin^2(pi*theta/2) should be uniform in [0,1]
+    s2pt2 = np.random.uniform(0, 1)
+    theta = 2 / np.pi * np.arcsin(np.sqrt(s2pt2))
+    phi = np.random.uniform(0, 2)
+    lamda = np.random.uniform(0, 2)
+    return (theta, phi, lamda)
+
+def random_init_params(num_qubits, depth):
+    """
+    Randomly initialize parameters for an ansatz circuit of the given depth.
+    In this initialization, the U3 and product state parameters are chosen
+    Haar-randomly, while the RZZ parameters are all set to 0.
+
+    Parameters
+    ----------
+    num_qubits : int
+        Number of qubits in the circuit.
+    depth : int
+        Depth of the ansatz circuit, as measured by number of two-qubit layers.
+
+    Returns
+    -------
+    jax array
+        Randomly generated initial parameters that can be used for optimization.
+        Array has length `num_qubits * 2 + num_gate_params(num_qubits, depth)`.
+    """
+
+    mod = depth_modulus(num_qubits)
+    num_rzz_params = (num_qubits // 2) * depth
+
+    # First generate the product parameters
+    # This means taking (theta, phi) but not lamda
+    product_params = np.concatenate([haar_u3_params()[:2] for i in range(n)])
+
+    # Then generate the U3 parameters
+    u3_params = np.concatenate([haar_u3_params() for i in range(2 * num_rzz_params)])
+    # Initialize all RZZ parameters to 0
+    rzz_params = np.zeros(num_rzz_params)
+
+    # Reshape into the appropriate number of blocks for the repeated circuit
+    u3_params_reshaped = u3_params.reshape((depth // depth_modulus, -1))
+    rzz_params_reshaped = rzz_params.reshape((depth // depth_modulus, -1))
+    # Stack RZZ and U3 parameters horizontally, then flatten
+    circ_params = np.hstack((rzz_params_reshaped, u3_params_reshaped)).flatten()
+    return np.concatenate((product_params, circ_params))
+
 def optimize(
     target_state,
     depth,
@@ -196,7 +258,7 @@ def optimize(
     total_num_params = num_qubits * 2 + num_gate_params(num_qubits, depth)
     # TODO do this with a seed
     if init_params is None:
-        init_params = np.random.normal(scale=0.2, size=total_num_params)
+        init_params = random_init_params(num_qubits, depth)
     init_params = jnp.array(init_params)
     if init_params.shape != (total_num_params,):
         raise Exception(
