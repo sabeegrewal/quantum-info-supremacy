8a5b466e3a1a8473cfd266e650065f65735a1311
Sabee Grewal <sabee@Sabees-MacBook-Pro-2.local>
2025-03-23 10:38:17 -0500

    Refactor to stitch distinct circuits
    We now accept a start_seed, n_jobs, and n_stitches. n_stitches many circuits are prepared, stitched into a single circuit, and submitted. This repeats until n_jobs many circuits have been prepared in total.

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ submit_job = True
 n_stitches = 5
 n_shots = 1
 start_seed = 0
-n_jobs = 10
+n_jobs = 3
 
 print("-" * 30)
 print(f"n               : {n}")
@@ -42,10 +42,9 @@ print(f"n_shots         : {n_shots}")
 print("")
 print(f"start_seed      : {start_seed}")
 print(f"n_jobs          : {n_jobs}")
-print(f"n_submissions   : {n_jobs//n_stitches}")
+print(f"n_submissions   : {int(np.ceil(n_jobs/n_stitches))}")
 print("-" * 30)
 
-# Prompt user
 while True:
     user_input = input("Enter 'Y' to continue or 'N' to exit: ").strip().lower()
     if user_input == "y":
@@ -57,35 +56,63 @@ while True:
     else:
         print("Invalid input. Please enter 'Y' or 'N'.")
 
+# Double check...
+if device_name == "H1-1":
+    print(f"Are you sure? You are submitting to the real machine!")
+    while True:
+        user_input = input("Enter 'Y' to continue or 'N' to exit: ").strip().lower()
+        if user_input == "y":
+            print("Continuing...")
+            break
+        elif user_input == "n":
+            print("Exiting...")
+            exit()
+        else:
+            print("Invalid input. Please enter 'Y' or 'N'.")
 
-for seed in range(1):
-    print(f"seed {seed}")
-    random_bits = rand.read_chunk(seed)
-    rand_gen = rand.TrueRandom(random_bits)
+
+for seed in range(start_seed, start_seed + n_jobs, n_stitches):
+    batch = list(range(seed, min(seed + n_stitches, start_seed + n_jobs)))
+    print(f"seeds: {batch}")
+    random_bits_list = [rand.read_chunk(s) for s in batch]
+    rand_gens = [rand.TrueRandom(random_bits) for random_bits in random_bits_list]
 
     # First do all of the randomness generation
-    target_state_r = rand_gen.normal(size=([2] * n))
-    target_state_i = rand_gen.normal(size=([2] * n))
-    target_state = (target_state_r + 1j * target_state_i) / 2 ** ((n + 1) / 2)
+    target_states_r = [rand_gen.normal(size=([2] * n)) for rand_gen in rand_gens]
+    target_states_i = [rand_gen.normal(size=([2] * n)) for rand_gen in rand_gens]
+    target_states = [
+        (target_state_r + 1j * target_state_i) / 2 ** ((n + 1) / 2)
+        for target_state_r, target_state_i in zip(target_states_r, target_states_i)
+    ]
 
-    ag_toggles = random_stabilizer_toggles_ag(n, rand_gen)
-    reversed_ag_toggles = list(reversed(ag_toggles))
+    ag_toggles_list = [
+        random_stabilizer_toggles_ag(n, rand_gen) for rand_gen in rand_gens
+    ]
+    reversed_ag_toggles_list = [
+        list(reversed(ag_toggles)) for ag_toggles in ag_toggles_list
+    ]
 
     # Optimize
-    start = time.time()
-    # Ensure initial parameters are chosen consistently pseudorandomly
-    np.random.seed(seed)
-    opt = optimize(target_state, depth, noisy=noisy)
-    opt_params = opt.x
+    output_states = []
+    opt_params_list = []
+    noisy_fidelities = []
+    for i, s in enumerate(batch):
+        start = time.time()
+        # Ensure initial parameters are chosen consistently pseudorandomly
+        np.random.seed(s)
+        opt = optimize(target_states[i], depth, noisy=noisy)
+        opt_params = opt.x
+        opt_params_list.append(opt_params)
 
-    output_state = output_state(n, opt_params)
-    noiseless_fidelity = -loss(opt_params, target_state)
-    fidelity_from_noise = fidelity_from_noise(n, opt_params)
-    print(f"Noiseless fidelity: {noiseless_fidelity}")
-    print(f"Estimated fidelity due to noise: {fidelity_from_noise}")
-    print(f"Estimated overall fidelity: {fidelity_from_noise * noiseless_fidelity}")
-    print(f"Optimization time: {time.time() - start}")
-    print("")
+        output_states.append(output_state(n, opt_params))
+        noiseless_fidelity = -loss(opt_params, target_states[i])
+        noisy_fidelity = fidelity_from_noise(n, opt_params)
+        noisy_fidelities.append(noisy_fidelity)
+        print(f"Noiseless fidelity: {noiseless_fidelity}")
+        print(f"Estimated fidelity due to noise: {noisy_fidelity}")
+        print(f"Estimated overall fidelity: {noisy_fidelity * noiseless_fidelity}")
+        print(f"Optimization time: {time.time() - start}")
+        print("")
 
     if device_name == "H1-1LE":
         api_offline = QuantinuumAPIOffline()
@@ -96,28 +123,50 @@ for seed in range(1):
             api_handler=QuantinuumAPI(token_store=QuantinuumConfigCredentialStorage()),
         )
 
-    state_prep_circ = make_ansatz_circuit(n, opt_params, method="pytket")
-    cliff_circ = make_clifford_circuit(n, reversed_ag_toggles, backend)
-    scoring_state = apply_clifford(target_state, reversed_ag_toggles)
-    cliff_output_state = apply_clifford(output_state, reversed_ag_toggles)
+    state_prep_circs = [
+        make_ansatz_circuit(n, opt_params, method="pytket")
+        for opt_params in opt_params_list
+    ]
+    cliff_circs = [
+        make_clifford_circuit(n, reversed_ag_toggles, backend)
+        for reversed_ag_toggles in reversed_ag_toggles_list
+    ]
+    scoring_states = [
+        apply_clifford(target_state, reversed_ag_toggles)
+        for target_state, reversed_ag_toggles in zip(
+            target_states, reversed_ag_toggles_list
+        )
+    ]
+    cliff_output_states = [
+        apply_clifford(output_state, reversed_ag_toggles)
+        for output_state, reversed_ag_toggles in zip(
+            output_states, reversed_ag_toggles_list
+        )
+    ]
+
+    basis_xebs = [
+        sum(abs((scoring_state * cliff_output_state).flatten() ** 2)) * 2**n - 1
+        for scoring_state, cliff_output_state in zip(
+            scoring_states, cliff_output_states
+        )
+    ]
+    print(f"Noiseless basis XEB: {[float(xeb) for xeb in basis_xebs]}")
+    print(
+        f"Est. noisy basis XEB: {[float(basis_xeb * nf) for basis_xeb, nf in zip(basis_xebs, noisy_fidelities)]}"
+    )
+    print("")
 
     overall_circ = stitch_circuits(
-        [state_prep_circ] * n_stitches,
-        [cliff_circ] * n_stitches,
+        state_prep_circs,
+        cliff_circs,
         backend,
         detect_leakage,
     )
-
-    basis_xeb = sum(abs((scoring_state * cliff_output_state).flatten() ** 2)) * 2**n - 1
-    print(f"Noiseless basis XEB: {basis_xeb}")
-    print(f"Est. noisy basis XEB: {basis_xeb * fidelity_from_noise}")
 
     if submit_job:
         if device_name == "H1-1LE":
             result = backend.run_circuit(overall_circ, n_shots=n_shots)
         else:
-            if device_name == "H1-1":
-                input("Submitting job to the real machine! Press enter to continue.")
             result_handle = backend.process_circuit(overall_circ, n_shots=n_shots)
             job_data = job_data.JobData(
                 n,
@@ -125,14 +174,14 @@ for seed in range(1):
                 noisy,
                 device_name,
                 detect_leakage,
-                [seed] * n_stitches,
-                [target_state] * n_stitches,
-                [reversed_ag_toggles] * n_stitches,
-                [opt_params] * n_stitches,
+                batch,
+                target_states,
+                reversed_ag_toggles_list,
+                opt_params_list,
                 overall_circ,
                 result_handle,
             )
             job_data.save()
             result = await_job(backend, result_handle)
 
-        print_results([scoring_state] * n_stitches, detect_leakage, result)
+        print_results(scoring_states, detect_leakage, result)
