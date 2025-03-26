43ba1e80a8eca61294839db50f584ad8511ccda4
Sabee Grewal <sabee@wireless-10-150-49-136.public.utexas.edu>
2025-03-26 13:30:07 -0500

    Add job_data.py, deleted by mistake

---
+++ utils/job_data.py
@@ -0,0 +1,124 @@
+from pytket import Circuit
+
+from pytket.backends.resulthandle import ResultHandle
+
+import time
+import json
+import numpy as np
+
+
+class JobData:
+    """Class for storing data associated with a job submission.
+
+    Parameters
+    ----------
+    n : int
+        Number of qubits.
+    depth : int
+        Depth of the state preparation circuit, as measured by 2-qubit gate layers.
+    noisy : bool
+        Whether the circuits were optimized using the noisy or noiseless loss function.
+    device_name : str
+        Device name, e.g. "H1-1", "H1-1E", "H1-1LE".
+    detect_leakage : bool
+        Whether the circuit uses the leakage detection gadget.
+    seeds : list[int]
+        List of seeds used for randomness in each of the trials.
+    target_states : list[array]
+        List of complex arrays of shape `[2] * n`.
+    reversed_ag_toggle_lists : list[list[bool]]
+        A list of which Clifford gates are toggled using Aaronson-Gottesman compilation of the measurement,
+        one for each trial.
+    opt_param_lists : list[array]
+        List of real arrays containing the optimized circuit parameters for each state.
+    overall_circ : Circuit
+        The circuit submitted to the backend.
+    result_handle : ResultHandle
+        Handle used to access the job result.
+    """
+
+    def __init__(
+        self,
+        n,
+        depth,
+        noisy,
+        device_name,
+        detect_leakage,
+        seeds,
+        target_states,
+        reversed_ag_toggle_lists,
+        opt_param_lists,
+        overall_circ,
+        result_handle,
+    ):
+        self.n = n
+        self.depth = depth
+        self.noisy = noisy
+        self.device_name = device_name
+        self.detect_leakage = detect_leakage
+        self.seeds = seeds
+        self.target_states = np.array(target_states)
+        self.reversed_ag_toggle_lists = reversed_ag_toggle_lists
+        self.opt_param_lists = np.array(opt_param_lists)
+        self.overall_circ = overall_circ
+        self.result_handle = result_handle
+
+    def save(self, filename=None):
+        """Save the data for this job submission locally so that it can be recovered later.
+
+        Parameters
+        ----------
+        filename : str
+            Optional filename to which the data should be saved. Defaults to a file location in the `job_handles`
+            folder with a name that is a concatenation of the number of qubits, depth, and current time.
+        """
+        if filename is None:
+            time_str = (
+                time.asctime().replace("/", "_").replace(":", "-").replace(" ", "_")
+            )
+            filename = f"job_handles/{self.n}_{self.depth}_{time_str}.txt"
+
+        file = open(filename, "w")
+        file.write(str(self.n) + "\n")
+        file.write(str(self.depth) + "\n")
+        file.write(str(self.noisy) + "\n")
+        file.write(self.device_name + "\n")
+        file.write(str(self.detect_leakage) + "\n")
+        file.write(str(self.seeds) + "\n")
+        file.write(str(self.target_states.tolist()) + "\n")
+        file.write(str(self.reversed_ag_toggle_lists) + "\n")
+        file.write(str(self.opt_param_lists.tolist()) + "\n")
+        file.write(json.dumps(self.overall_circ.to_dict()) + "\n")
+        file.write(str(self.result_handle) + "\n")
+        file.close()
+
+    @staticmethod
+    def load(filename):
+        file = open(filename, "r")
+
+        n = int(file.readline()[:-1])
+        depth = int(file.readline()[:-1])
+        noisy = file.readline()[:-1] == "True"
+        device_name = file.readline()[:-1]
+        detect_leakage = file.readline()[:-1] == "True"
+        # TODO ideally something safer than running eval()...
+        seeds = eval(file.readline()[:-1])
+        target_states = np.array(eval(file.readline()[:-1]))
+        reversed_ag_toggle_lists = eval(file.readline()[:-1])
+        opt_param_lists = np.array(eval(file.readline()[:-1]))
+        overall_circ = Circuit.from_dict(json.loads(file.readline()[:-1]))
+        result_handle = ResultHandle.from_str(file.readline()[:-1])
+
+        return JobData(
+            n,
+            depth,
+            noisy,
+            device_name,
+            detect_leakage,
+            seeds,
+            target_states,
+            reversed_ag_toggle_lists,
+            opt_param_lists,
+            overall_circ,
+            result_handle,
+        )
