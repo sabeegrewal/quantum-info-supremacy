467fa5e92b144312f76bdf00230971fd0f3796c2
William Kretschmer <william-kretschmer@comcast.net>
2025-03-26 12:15:10 -0700

    Remove extra space

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ if __name__ == "__main__":
     print(f"n_shots         : {n_shots}")
     print("")
     print(f"start_seed      : {start_seed}")
-    print(f"n_seeds          : {n_seeds}")
+    print(f"n_seeds         : {n_seeds}")
     print(f"n_submissions   : {int(np.ceil(n_seeds/n_stitches))}")
     print("-" * 30)
 
