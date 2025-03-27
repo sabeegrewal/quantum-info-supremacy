Fix computation of n_submissions
I'm worried about potential floating point errors if the numbers get too large. Should be safe now.

main.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
     print("")
     print(f"start_seed      : {start_seed}")
     print(f"n_seeds         : {n_seeds}")
-    print(f"n_submissions   : {int(np.ceil(n_seeds/n_stitches))}")
+    print(f"n_submissions   : {n_seeds // n_stitches + bool(n_seeds % n_stitches)}")
     print("-" * 30)
 
     while True: