08a0c9477733f3ff4a2820f03ee4a82e84d3b1e4
William Kretschmer <william-kretschmer@comcast.net>
2025-07-16 23:15:27 -0700

    Include both Haar and Clifford in noiseless bound

--- figures/noiseless_bound.py
+++ figures/noiseless_bound.py
@@ -17,12 +17,15 @@ myrange = range(4, 17)
 plt.rcParams['font.family'] = 'Arial'
 plt.rcParams['font.size'] = 12
 
-cliff_comm = [min_communication(1.0, n, "haar") for n in myrange]
+haar_comm = [min_communication(1.0, n, "haar") for n in myrange]
+cliff_comm = [min_communication(1.0, n, "cliff") for n in myrange]
 hm_comm = [min_communication_hm(1.0, n) for n in myrange]
+plt.plot(myrange, haar_comm, color="#30A08E", linestyle=":",
+         linewidth=1.5, marker="o", label="DXHOG, Haar")
 plt.plot(myrange, cliff_comm, color="#30A08E", linestyle=":",
-         linewidth=1.5, marker="o", label="DXHOG")
+         linewidth=1.5, marker="s", label="DXHOG, Clifford")
 plt.plot(myrange, hm_comm, color="#8064A2", linestyle=":",
-         linewidth=1.5, marker="s", label="HM")
+         linewidth=1.5, marker="o", label="HM")
 plt.xlabel("Number of noiseless qubits, $n$")
 plt.ylabel("Lower bound on bits of classical communication to spoof, $m$")
 plt.yscale("log", base=2)
