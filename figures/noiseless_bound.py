bc4289c9f4578fd8f40da2f675c9e1d689b555f3
William Kretschmer <william-kretschmer@comcast.net>
2025-07-17 12:05:01 -0700

    Style updates and trivial bound for noiseless plot

--- figures/noiseless_bound.py
+++ figures/noiseless_bound.py
@@ -13,6 +13,7 @@ def min_communication_hm(fidelity, n):
     return fidelity * np.sqrt(2**n - 1) / 2 - 1
 
 myrange = range(4, 17)
+lrange = np.arange(4.0, 16.01, 0.1)
 
 plt.rcParams['font.family'] = 'Arial'
 plt.rcParams['font.size'] = 12
@@ -20,16 +21,22 @@ plt.rcParams['font.size'] = 12
 haar_comm = [min_communication(1.0, n, "haar") for n in myrange]
 cliff_comm = [min_communication(1.0, n, "cliff") for n in myrange]
 hm_comm = [min_communication_hm(1.0, n) for n in myrange]
-plt.plot(myrange, haar_comm, color="#30A08E", linestyle=":",
-         linewidth=1.5, marker="o", label="DXHOG, Haar")
-plt.plot(myrange, cliff_comm, color="#30A08E", linestyle=":",
-         linewidth=1.5, marker="s", label="DXHOG, Clifford")
-plt.plot(myrange, hm_comm, color="#8064A2", linestyle=":",
-         linewidth=1.5, marker="o", label="HM")
+plt.plot(lrange, lrange, color="gray", linestyle=":",
+         linewidth=1.5, label="$m = n$")
+plt.plot(myrange, haar_comm, color="#30A08E", linestyle="--",
+         linewidth=1, marker="o", label="DXHOG, Haar")
+plt.plot(myrange, cliff_comm, color="#30A08E", linestyle="--",
+         linewidth=1, marker="s", label="DXHOG, Clifford")
+plt.plot(myrange, hm_comm, color="#8064A2", linestyle="--",
+         linewidth=1, marker="o", label="HM")
 plt.xlabel("Number of noiseless qubits, $n$")
 plt.ylabel("Lower bound on bits of classical communication to spoof, $m$")
 plt.yscale("log", base=2)
-plt.legend()
+
+# Ensure proper axis ordering
+handles, labels = plt.gca().get_legend_handles_labels()
+order = [1, 2, 3, 0]
+plt.legend([handles[i] for i in order], [labels[i] for i in order])
 plt.grid(True, which="both", linestyle="--", linewidth=0.5, alpha=0.7)
 
 plt.savefig("noiseless_bound.pdf")
