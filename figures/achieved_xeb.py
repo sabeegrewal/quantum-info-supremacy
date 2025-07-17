bace24b58ffb8411620efee965c042a8faa2a171
William Kretschmer <william-kretschmer@comcast.net>
2025-07-16 23:06:34 -0700

    Figures that involve XEB bounds

---
+++ figures/achieved_xeb.py
@@ -0,0 +1,39 @@
+from classical_bounds import *
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Plot the n=12 bound along with the XEB observed in experiment
+
+myrange = np.arange(0, 250, 1)
+
+# mu at ~77.29 bits
+# mu - 5 sigma at ~61.37 bits
+mu = 0.4267625296473500
+sigma = 1.3058928853745300 / np.sqrt(10000)
+
+plt.rcParams['font.family'] = 'Arial'
+plt.rcParams['font.size'] = 12
+
+xebs = [max_classical_xeb_jit(x, 12, "cliff") for x in myrange]
+plt.plot(myrange, xebs, color="#30A08E", linewidth=2, label="Classical bound")
+plt.xlabel("Bits of classical communication, $m$")
+plt.ylabel("Maximum achievable XEB, $\epsilon$")
+plt.xlim((0,250))
+plt.ylim((0,1))
+
+# 0.426762529
+plt.hlines(mu, 0, 250, colors='#8064A2', linestyles='dotted', linewidth=1.5, label=r"Observed $\mu$")
+# 0.361467885
+plt.hlines(mu - 5 * sigma, 0, 250, colors='#E75D72', linestyles='dotted', linewidth=1.5, label=r"Observed $\mu - 5\sigma$")
+
+plt.annotate("$m$ = 77.3", xy=(77.29, mu), xytext=(77.29, mu+0.15),
+            arrowprops=dict(arrowstyle='->'), ha='center')
+plt.annotate("$m$ = 61.4", xy=(61.37, mu - 5 * sigma), xytext=(61.37, mu - 5*sigma - 0.15),
+            arrowprops=dict(arrowstyle='->'), ha='center')
+
+plt.legend()
+
+plt.savefig("achieved_xeb.pdf")
+plt.show()
---
+++ figures/noiseless_bound.py
@@ -0,0 +1,33 @@
+from classical_bounds import *
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Plot the noiseless lower bound as a function of n
+
+def max_classical_hm_fidelity(m, n):
+    return 2 * (m + 1) / np.sqrt(2**n - 1)
+
+def min_communication_hm(fidelity, n):
+    return fidelity * np.sqrt(2**n - 1) / 2 - 1
+
+myrange = range(4, 17)
+
+plt.rcParams['font.family'] = 'Arial'
+plt.rcParams['font.size'] = 12
+
+cliff_comm = [min_communication(1.0, n, "haar") for n in myrange]
+hm_comm = [min_communication_hm(1.0, n) for n in myrange]
+plt.plot(myrange, cliff_comm, color="#30A08E", linestyle=":",
+         linewidth=1.5, marker="o", label="DXHOG")
+plt.plot(myrange, hm_comm, color="#8064A2", linestyle=":",
+         linewidth=1.5, marker="s", label="HM")
+plt.xlabel("Number of noiseless qubits, $n$")
+plt.ylabel("Lower bound on bits of classical communication to spoof, $m$")
+plt.yscale("log", base=2)
+plt.legend()
+plt.grid(True, which="both", linestyle="--", linewidth=0.5, alpha=0.7)
+
+plt.savefig("noiseless_bound.pdf")
+plt.show()
