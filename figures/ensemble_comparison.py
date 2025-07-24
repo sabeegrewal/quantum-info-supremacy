69384ce44a4f834ac390ca68ab15869d885d472c
William Kretschmer <william-kretschmer@comcast.net>
2025-07-24 10:48:41 -0400

    Add classically achievable XEB to figure

--- figures/ensemble_comparison.py
+++ figures/ensemble_comparison.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 # Plot the n=12 bound for different unitary ensembles
 
-myrange = np.arange(0, 400, 1)
+myrange = list(range(401))
 
 
 plt.rcParams['font.family'] = 'Arial'
@@ -15,21 +15,23 @@ plt.rcParams['font.size'] = 12
 haar_xebs = [max_classical_xeb_jit(x, 12, "haar") for x in myrange]
 design_xebs = [max_classical_xeb_jit(x, 12, "design") for x in myrange]
 cliff_xebs = [max_classical_xeb_jit(x, 12, "cliff") for x in myrange]
+lb_xebs = [achievable_classical_xeb(x, 12) for x in myrange]
 
 #prod_xebs = [max_classical_xeb_jit(x, 12, "cliff_prod") for x in myrange[:11]]
 plt.plot(myrange, haar_xebs, color="#E75D72", linewidth=1.5, linestyle="--", label="Haar")
 plt.plot(myrange, design_xebs, color="#8064A2", linewidth=1.5, linestyle="-.", label="$10$-design")
 plt.plot(myrange, cliff_xebs, color="#30A08E", linewidth=1.5, label="Clifford")
+plt.plot(myrange, lb_xebs, color="gray", linewidth=1.5, linestyle = ":", label="Classical protocol")
 #plt.plot(myrange[:11], prod_xebs, color="#FF9A56", linewidth=2, label="Product Clifford")
 plt.xlabel("Bits of classical communication, $m$")
-plt.ylabel("Maximum achievable XEB, $\epsilon$")
-plt.xlim((0,375))
+plt.ylabel("Bound on $\mathcal{F}_{\mathrm{XEB}}$, $\epsilon$")
+plt.xlim((0,400))
 plt.ylim((0,1))
 plt.legend()
 
 # Ensure proper axis ordering
 handles, labels = plt.gca().get_legend_handles_labels()
-order = [2, 1, 0]
+order = [2, 1, 0, 3]
 plt.legend([handles[i] for i in order], [labels[i] for i in order])
 plt.grid(True, which="both", linestyle="--", linewidth=0.5, alpha=0.7)
 
