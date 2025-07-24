ee3b9b2eec424ac250313bc51397775505e2eb1a
William Kretschmer <william-kretschmer@comcast.net>
2025-07-24 10:49:02 -0400

    Transpose x and y axes on achieved XEB figure

--- figures/achieved_xeb.py
+++ figures/achieved_xeb.py
@@ -17,20 +17,20 @@ plt.rcParams['font.family'] = 'Arial'
 plt.rcParams['font.size'] = 12
 
 xebs = [max_classical_xeb_jit(x, 12, "cliff") for x in myrange]
-plt.plot(myrange, xebs, color="#30A08E", linewidth=2, label="Classical bound")
-plt.xlabel("Bits of classical communication, $m$")
-plt.ylabel("Maximum achievable XEB, $\epsilon$")
-plt.xlim((0,250))
-plt.ylim((0,1))
+plt.plot(xebs, myrange, color="#30A08E", linewidth=2, label="Classical bound")
+plt.ylabel("Bits of classical communication, $m$")
+plt.xlabel("Maximum achievable $\mathcal{F}_{\mathrm{XEB}}$")
+plt.ylim((0,250))
+plt.xlim((0,1))
 
 # 0.426762529
-plt.hlines(mu, 0, 250, colors='#8064A2', linestyles='-.', linewidth=1.5, label=r"Observed $\mu$")
+plt.vlines(mu, 0, 250, colors='#8064A2', linestyles='-.', linewidth=1.5, label=r"Observed $\mu$")
 # 0.361467885
-plt.hlines(mu - 5 * sigma, 0, 250, colors='#E75D72', linestyles='--', linewidth=1.5, label=r"Observed $\mu - 5\sigma$")
+plt.vlines(mu - 5 * sigma, 0, 250, colors='#E75D72', linestyles='--', linewidth=1.5, label=r"Observed $\mu - 5\sigma$")
 
-plt.annotate("$m$ = 77.3", xy=(77.29, mu), xytext=(77.29, mu+0.15),
+plt.annotate("$m$ = 77.3", xy=(mu, 77.29), xytext=(mu+0.15, 77.29),
             arrowprops=dict(arrowstyle='->'), ha='center')
-plt.annotate("$m$ = 61.4", xy=(61.37, mu - 5 * sigma), xytext=(61.37, mu - 5*sigma - 0.15),
+plt.annotate("$m$ = 61.4", xy=(mu - 5 * sigma, 61.37), xytext=(mu - 5*sigma - 0.15, 61.37),
             arrowprops=dict(arrowstyle='->'), ha='center')
 
 plt.legend()
