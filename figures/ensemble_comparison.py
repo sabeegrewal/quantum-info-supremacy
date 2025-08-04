929dd7108bf46113c0d332ae33204398dbaa23d3
William Kretschmer <william-kretschmer@comcast.net>
2025-08-04 14:03:57 -0700

    Update colors and transpose comparison figure

--- figures/ensemble_comparison.py
+++ figures/ensemble_comparison.py
@@ -18,20 +18,20 @@ cliff_xebs = [max_classical_xeb_jit(x, 12, "cliff") for x in myrange]
 lb_xebs = [achievable_classical_xeb(x, 12) for x in myrange]
 
 #prod_xebs = [max_classical_xeb_jit(x, 12, "cliff_prod") for x in myrange[:11]]
-plt.plot(myrange, haar_xebs, color="#E75D72", linewidth=1.5, linestyle="--", label="Haar")
-plt.plot(myrange, design_xebs, color="#8064A2", linewidth=1.5, linestyle="-.", label="$10$-design")
-plt.plot(myrange, cliff_xebs, color="#30A08E", linewidth=1.5, label="Clifford")
-plt.plot(myrange, lb_xebs, color="gray", linewidth=1.5, linestyle = ":", label="Classical protocol")
+plt.plot(haar_xebs, myrange, color="gray", linewidth=1.5, linestyle=(5, (10, 3)), label="Haar")
+plt.plot(design_xebs, myrange, color="#E75D72", linewidth=1.5, linestyle="-.", label="$10$-design")
+plt.plot(cliff_xebs, myrange, color="#8064A2", linewidth=1.5, linestyle="--", label="Clifford")
+plt.plot(lb_xebs, myrange, color="#30A08E", linewidth=1.5, linestyle = "-", label="Classical protocol")
 #plt.plot(myrange[:11], prod_xebs, color="#FF9A56", linewidth=2, label="Product Clifford")
-plt.xlabel("Bits of classical communication, $m$")
-plt.ylabel("Bound on $\mathcal{F}_{\mathsf{XEB}}$, $\epsilon$")
-plt.xlim((0,400))
-plt.ylim((0,1))
+plt.ylabel("Bits of classical communication, $m$")
+plt.xlabel("Bound on $\mathcal{F}_{\mathsf{XEB}}$, $\epsilon$")
+plt.ylim((0,400))
+plt.xlim((0,1))
 plt.legend()
 
 # Ensure proper axis ordering
 handles, labels = plt.gca().get_legend_handles_labels()
-order = [2, 1, 0, 3]
+order = [3, 0, 1, 2]
 plt.legend([handles[i] for i in order], [labels[i] for i in order])
 plt.grid(True, which="both", linestyle="--", linewidth=0.5, alpha=0.7)
 
